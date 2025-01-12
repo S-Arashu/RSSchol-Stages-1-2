f9161fa0ae1660da63b2b8fd194a579d3e15cf95
Anastasiya Tomasheva <siha.sole@gmail.com>
2025-01-12 13:22:21 +0300

    feat: add start page

---
+++ simon-says/index.js
@@ -0,0 +1,21 @@
+document.body.innerHTML = `
+  <div class="level">
+    <p class="level-title">
+      Choose your level!
+    </p>
+    <div class="container-for-level-choose">
+      <div class="level-choose">
+        <span class="level-choose-name">Easy</span>
+      </div>
+      <div class="level-choose">
+        <span class="level-choose-name">Medium</span>
+      </div>
+      <div class="level-choose">
+        <span class="level-choose-name">Hard</span>
+      </div>
+    </div>
+  </div>
+  <div class="start-game">
+    <span>Start</span>
+  </div>
+`
