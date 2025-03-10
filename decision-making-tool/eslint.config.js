506b2cb78120f1a8f33d39a5159d1b33d2172f35
Anastasiya Tomasheva <siha.sole@gmail.com>
2025-03-11 02:00:45 +0300

    feat: setup configuration files

---
+++ decision-making-tool/eslint.config.js
@@ -0,0 +1,24 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintPluginUnicorn from 'eslint-plugin-unicorn';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: globals.builtin,
+    },
+    plugins: {
+      unicorn: eslintPluginUnicorn,
+    },
+    rules: {
+      'unicorn/better-regex': 'error',
+      'unicorn/…': 'error',
+    },
+  },
+];
---
+++ decision-making-tool/src/index.ts
@@ -0,0 +1,5 @@
+console.log('Hi');
+
+console.log('Hey');
+console.log('Go!');
+console.log('Boo');
