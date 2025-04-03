b72c2d97e5a27d3d51c20be5f64060faa4372821
Anastasiya Tomasheva <siha.sole@gmail.com>
2025-04-03 10:48:36 +0300

    feat: add button to modal window

--- decision-making-tool/src/builders/dialog.ts
+++ decision-making-tool/src/builders/dialog.ts
@@ -1,13 +1,22 @@
 export function dialogWrongValue(parent: { append: (arg0: HTMLDialogElement) => void }) {
   const dialog = document.createElement('dialog');
-  dialog.classList.add('popup');
+  dialog.classList.add('popup-wrong');
   dialog.textContent = 'Please, enter correct data';
   parent.append(dialog);
+  const dialogButton = document.createElement('button');
+  dialogButton.classList.add('dialog-button');
+  dialogButton.innerText = 'OK';
+  dialog.append(dialogButton);
   dialog.showModal();
 
   dialog.addEventListener('cancel', event => {
     // if (event.key === 'Escape') {
     dialog.remove();
     // }
+  });
+
+  dialogButton.addEventListener('click', () => {
+    dialog.close();
+    dialog.remove();
   });
 }
